38128dd567c87afa0360a9e1cb931e0d07b12ef0
Bigfredo270 <baahfrederick08@gmail.com>
2024-06-10 15:08:39 +0000

    project setup

---
+++ CSM 158-1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int age;
+ int year =2024;
+ float score;
+ bool available;
+
+ char gender;
+ char grade ='A';
+
+ const double ssalary =2000000;
+
+ cout <<"Enter your age:"<<endl;
+ cin >>age;
+ cout << age;
+
+    return 0;
+}
---
+++ Forloops.cpp/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int number;
+int sum
+cout<< ""
+for(int I=0; I=I);
+        cout<< "Number: "<<count<<endl;
+        count++;
+   count<<count++endl;
+   }
+    return 0;
+}
